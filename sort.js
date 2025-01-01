96a3bec4cc6c01866814f4f16ad431b07ed6fe26
Carlos Solis <30050682+mmgcarlos@users.noreply.github.com>
2025-01-01 21:48:34 +0100

    Selection sort

--- sort.js
+++ sort.js
@@ -8,3 +8,16 @@ const bubbleSort = (arr, from = 0, to = arr.length - 1) => {
   }
   return arr;
 };
+
+const selectionSort = (arr, from = 0, to = arr.length - 1) => {
+  for (let i = from; i < to; i++) {
+    let min = i;
+    for (let j = i + 1; j <= to; j++) {
+      if (arr[j] < min) {
+        min = j;
+      }
+    }
+    [arr[i],arr[min]] = [arr[min], arr[i]];
+  }
+  return arr;
+};
