2dfdc4edc60adb6fea84ab832b00f5c95c63031a
Carlos Solis <30050682+mmgcarlos@users.noreply.github.com>
2025-01-02 18:04:04 +0100

    Create missingPair.test.js

---
+++ tests/missingPair.test.js
@@ -0,0 +1,8 @@
+const { missingPair } = require('../src/missingPair');
+
+describe('Find missing pair', () => {
+
+  test('There are no missing pairs', () => {
+    expect(missingPair([5, 9, 6, 6, 9, 5])).toEqual([]);
+  });
+});
