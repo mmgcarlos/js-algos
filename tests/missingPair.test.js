e50834e58b0c0685aa14e9d0d4f92e1306c6b43b
Carlos Solis <30050682+mmgcarlos@users.noreply.github.com>
2025-01-02 18:14:43 +0100

    Case of array size 1

--- tests/missingPair.test.js
+++ tests/missingPair.test.js
@@ -9,4 +9,8 @@ describe('Find missing pair', () => {
   test('There are no missing pairs in []', () => {
     expect(missingPair([])).toEqual([]);
   });
+
+  test('Missing pair in array size 1', () => {
+    expect(missingPair([5])).toEqual([5, 0]);
+  });
 });
