8793dbb19f230de00557a34c802191e9c028dd3b
Carlos Solis <30050682+mmgcarlos@users.noreply.github.com>
2025-01-02 18:06:34 +0100

    MissingPair empty case

--- tests/missingPair.test.js
+++ tests/missingPair.test.js
@@ -5,4 +5,8 @@ describe('Find missing pair', () => {
   test('There are no missing pairs', () => {
     expect(missingPair([5, 9, 6, 6, 9, 5])).toEqual([]);
   });
+
+  test('There are no missing pairs in []', () => {
+    expect(missingPair([])).toEqual([]);
+  });
 });
