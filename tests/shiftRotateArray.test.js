226e9d3512b0a138232be773fec494d71afef640
Carlos Solis <30050682+mmgcarlos@users.noreply.github.com>
2025-01-14 22:39:06 +0000

    Reverse array cases

--- tests/shiftRotateArray.test.js
+++ tests/shiftRotateArray.test.js
@@ -6,4 +6,12 @@ describe('Reverse array', () => {
     expect(reverse([])).toEqual([]);
   });
 
+  test('Reverse array size 1', () => {
+    expect(reverse([1])).toEqual([1]);
+  });
+
+  test('Reverse array', () => {
+    expect(reverse([1, 2, 3])).toEqual([3, 2, 1]);
+  });
+
 });
