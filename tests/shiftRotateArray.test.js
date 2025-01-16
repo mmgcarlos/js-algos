65d00c5f8450146cd97dac20caf173fcd34b019a
Carlos Solis <30050682+mmgcarlos@users.noreply.github.com>
2025-01-16 00:04:29 +0000

    Rotate negative

--- tests/shiftRotateArray.test.js
+++ tests/shiftRotateArray.test.js
@@ -46,4 +46,12 @@ describe('Rotate array', () => {
     expect(rotate([1, 2, 3, 4, 5], 13)).toEqual([3, 4, 5, 1, 2]);
   });
 
+  test('Rotate -1 position', () => {
+    expect(rotate([1, 2, 3], 1)).toEqual([2, 3, 1]);
+  });
+
+  test('Rotate -3 positions', () => {
+    expect(rotate([1, 2, 3, 4, 5], 3)).toEqual([4, 5, 1, 2, 3]);
+  });
+
 });
